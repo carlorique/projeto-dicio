0dc2c14de3cb4b2fd9b680fa8ba88c376e7de700
carlorique <carlos38080@gmail.com>
2024-03-11 17:54:11 -0400

    implementando funcinalidade no header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  palavra: string = '';
+  result: any;
 
+  constructor(private http: HttpClient) { }
+
+  async searchWord() {
+    if (!this.palavra) {
+      alert('Por favor, digite uma palavra.');
+      return;
+    }
+
+    try {
+      const encodedWord = encodeURIComponent(this.palavra);
+      const response = await this.http.get(`http://localhost:3000/search/${encodedWord}`).toPromise();
+      this.result = response;
+      console.log(this.result); // Verifica se os resultados foram obtidos corretamente
+    } catch (error) {
+      console.error(error);
+      alert('Erro ao buscar informações da palavra.');
+    }
+  }
+
+  speakWord() {
+    if (!this.palavra) {
+      alert('Por favor, digite uma palavra.');
+      return;
+    }
+
+    const speechSynthesis = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(this.palavra);
+
+    speechSynthesis.speak(utterance);
+  }
 }
